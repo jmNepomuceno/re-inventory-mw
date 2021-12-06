1bac70c150215449564b44823eb5179f024d22c7
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-12-07 02:38:09 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Components
+import Header from './components/Header'
+
+class App extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Header />
+            </React.Fragment>
+        )
+    }
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () =>{
+    return(
+        <header>
+            <img src={require('../imgs/index_imgs/bpsuLogo.png').default} alt="bpsu-logo" />
+            <h1> 
+                <span>I</span>NVENTORY <br/> <span>M</span>ANAGEMENT <span>W</span>EBSITE
+            </h1>
+            <img src={require('../imgs/index_imgs/inventory.png').default} alt="inventory-icon" />
+            <h1>Monkey</h1>
+        </header>
+    )
+}
+
+export default Header
